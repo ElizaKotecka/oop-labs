ce3adde7f12a81d56a173a925ae65795ce342e36
Eliza Kotecka <elizaganca@gmail.com>
2025-12-28 20:51:47 +0100

    SimWeb

---
+++ SimWeb/Program.cs
@@ -0,0 +1,45 @@
+namespace SimWeb;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        // obsluga sesji w pamieci
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        // uruchamiamy sesje
+        app.UseSession();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
